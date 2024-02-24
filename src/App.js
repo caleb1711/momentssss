846c86437c9a412afa47152c76f392f4b40243cd
caleb kakra Awuah <calkra15@outlook.com>
2024-02-24 22:18:07 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import styles from './App.module.css';
+import NavBar from './components/NavBar';
+import Container  from "react-bootstrap/Container";
+import {Route, Switch} from "react-router-dom";
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>God is GOD!</h1>
+    <div className={styles.App}>
+      <NavBar/>
+      <Container className={styles.Christ}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1> }/>
+        </Switch>
+        <switch>
+          <Route exact path="/signin" render={() => <h1>Sign in</h1> }/>
+        </switch>
+        <switch>
+          <Route exact path="/signup" render={() => <h1>Sign up</h1> }/>
+        </switch>
+        
+
+        
+      </Container>
     </div>
   );
 }
